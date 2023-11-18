d48497a45edf27acaf568f514ba9344f70f1b9cc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-18 21:57:38 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div>
       <div>
-        <DocumentMagnifyingGlassIcon className="w-64 h-64 text-in" />
+        <DocumentMagnifyingGlassIcon className="w-64 h-64 text-ind" />
       </div>
     </div>
   )
