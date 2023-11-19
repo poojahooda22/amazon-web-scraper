2a57d010f0695e53dcef0ce1824c85563f68658a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-19 15:34:35 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,24 @@ import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
 function HomePage() {
   return (
     <div>
-      <div>
-        <DocumentMagnifyingGlassIcon className="w-64 h-64 text-indigo-600/20" />
+      <div className="flex flex-col items-center justify-center">
+        <DocumentMagnifyingGlassIcon 
+          className="w-64 h-64 text-indigo-600/20" 
+        />
+        <h1
+          className="text-3xl font-bold text-center text-black mb-5"
+        >
+          Welcome to the Amazon Web Scraper
+        </h1>
+
+        <h2 className="text-lg italic text-center text-black/50">
+          Effortlessly extract product data and insights.
+          Maximize your e-commerce strategy with precision and ease.
+        </h2>
+
+        <h3 className="text-lg text-center italic text-black/50">
+          Get started by entering a product URL.
+        </h3>
       </div>
     </div>
   )
