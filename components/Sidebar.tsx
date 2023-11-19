97b0473a990f00edc80f84d718e4fde7bd750442
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-19 22:10:29 +0530

    updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,7 @@ function Sidebar() {
     return (
         <div>
             <div>
-                <DocumentMagnifyingGlassIcon className="h-16"
+                <DocumentMagnifyingGlassIcon className="h-16" md:
             </div>
             Sidebar
         </div>
