564229fc8612767b5b20b45eea3aea139bc2707e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-19 22:24:25 +0530

    updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,6 +9,7 @@ function Sidebar() {
                 <DocumentMagnifyingGlassIcon className="h-16 md:w-16 text-indigo-600" />
 
                 <h1>Web Scraper</h1>
+                <h2></h2>
             </div>
             Sidebar
         </div>
