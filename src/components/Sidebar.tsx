e39659a7621c37d0961721d52470a49272914d1f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 10:50:01 +0530

    added some classnames

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,10 +5,13 @@ import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
 function Sidebar() {
     return (
         <div 
-            className="p-2 md:p-10 py-6 overflow-y-auto 
-            border-b border-indigo-400/20"
+            className="p-2 md:p-10 py-6 overflow-y-auto bg-slate-100
+            border-b border-indigo-400/20 "
         >
-            <div className="flex flex-col items-center justify-center mb-10">
+            <div 
+                className="flex flex-col items-center 
+                justify-center mb-10"
+            >
                 <DocumentMagnifyingGlassIcon 
                     className="md:w-16 h-16 text-indigo-600" 
                 />
