cc54561378f4c005a2159635ba23bf396b8bdb40
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 18:37:52 +0530

    create route

---
+++ src/app/activateScraper/route.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest } from "next";
+
+// type Body = {
+//     search: string;
+// }
+
+// export async function POST(req: NextApiRequest) {
+//     const search = req.body.search;
+// }
+
