8a9428426a311639f0af71bfad791dcb456da15b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 10:01:25 +0530

    updated sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,12 +5,17 @@ import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
 function Sidebar() {
     return (
         <div>
-            <div>
-                <DocumentMagnifyingGlassIcon className="h-16 md:w-16 text-indigo-600" />
+            <div className="">
+                <DocumentMagnifyingGlassIcon 
+                    className="w-64 h-64 text-indigo-600/20" 
+                />
 
-                <h1>Web Scraper</h1>
-                <h2>Scraping the UnScrapable</h2>
+                <h1 className="hidden md:inline text-3xl mt-2">Web Scraper</h1>
+                <h2 className="hidden  md:inline text-xs italic">Scraping the UnScrapable</h2>
             </div>
+
+
+            <ul></ul>
             Sidebar
         </div>
     )
