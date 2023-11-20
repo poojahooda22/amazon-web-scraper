2bd1302c68f198e9ffc28354639ee659c35a3b89
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 11:05:12 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,16 @@ import React from 'react'
 function Header() {
   return (
     <header>
-        <form>
-            <input>
+        <form 
+            className='flex items-center space-x-2 justify-center 
+            rounded-full py-2 px-4 bg-indigo-100 max-w-md mx-auto'
+        >
+            <input
+                type="text"
+                placeholder="Search..."
+                className='flex-1'
+            />
             
-            </input>
             <button hidden>Search</button>
             <MagnifyingGlassIcon className='h-6 w-6 text-indigo-300' />
         </form>
