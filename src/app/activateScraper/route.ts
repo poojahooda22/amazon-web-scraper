a60ce91975e1afc51e34ecc00f8599beed180941
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 18:39:58 +0530

    updated

--- src/app/activateScraper/route.ts
+++ src/app/activateScraper/route.ts
@@ -4,7 +4,4 @@ type Body = {
     search: string;
 }
 
-// export async function POST(req: NextApiRequest) {
-//     const search = req.body.search;
-// }
-
+export async function POST(req: NextApiRequest) 
