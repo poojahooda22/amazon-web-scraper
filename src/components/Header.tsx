fea7b5daf29a630429c171b499fa13a8ec2eb320
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 18:38:03 +0530

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,47 @@
+'use client'
+
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import React from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 function Header() {
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const input = inputRef.current?.value;
+
+        if(!input) return;
+
+        if(inputRef.current?.value) {
+            inputRef.current.value = "";
+        }
+
+        try {
+            // call our API to activate the Scraper...
+            // /api/activeScraper
+        } catch (error) {
+            //handle any errors
+        }
+
+        //wait for the response to come back
+    };
+
   return (
     <header>
         <form 
+            onSubmit={handleSearch}
             className='flex items-center space-x-2 justify-center 
             rounded-full py-2 px-4 bg-indigo-100 max-w-md mx-auto'
         >
             <input
                 type="text"
                 placeholder="Search..."
-                className='flex-1 outline-none bg-transparent'
+                className='flex-1 outline-none 
+                bg-transparent placeholder:text-indigo-300'
+                ref={inputRef}
+                
             />
             
             <button hidden>Search</button>
