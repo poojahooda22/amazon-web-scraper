7d2641f97d966a613f7890e8e89b69bf7ee2efe3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 10:16:35 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
 import './globals.css'
-import Sidebar from '../../components/Sidebar'
+import Sidebar from '../components/Sidebar';
 
 const inter = Nunito_Sans({ subsets: ['latin'] })
 
