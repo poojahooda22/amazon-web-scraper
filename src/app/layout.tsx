181e6cc6bf7bcb40d0e86a8cd5405bb29be2f0f9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-20 10:32:51 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="flex h-screen bg-slate-100">
 
         {/* Sidebar */}
         <Sidebar />
