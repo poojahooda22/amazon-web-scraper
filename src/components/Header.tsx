523801be89ff48c75a6ff5da6a2c933a3db295ff
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-21 22:05:22 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ function Header() {
                 type="text"
                 placeholder="Search..."
                 className='flex-1 outline-none 
-                bg-transparent pla'
+                bg-transparent place'
                 ref={inputRef}
                 
             />
