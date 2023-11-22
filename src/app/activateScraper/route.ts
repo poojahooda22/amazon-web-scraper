b12dac7cbf0eee66f68d9bf51d1b7dfe8a68cb1f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-22 16:07:56 +0530

    updated

--- src/app/activateScraper/route.ts
+++ src/app/activateScraper/route.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest } from "next";
 
-type Body = {
-    search: string;
-}
+// type Body = {
+//     search: string;
+// }
 
 export async function POST(req: NextApiRequest) {
     const search = req.body.search;
