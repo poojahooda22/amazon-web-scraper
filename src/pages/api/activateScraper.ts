8a04910288070659995860af33a26511901cfc07
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-22 16:03:11 +0530

    updated scraper

--- src/pages/api/activateScraper.ts
+++ src/pages/api/activateScraper.ts
@@ -1,2 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+type Data = {
+    collection_id: string;
+    start_eta: number;
+};
+
+type Error = {
+    error: string;
+};
+
+export default function handler (
+    req: NextApiRequest,
+    res: NextApiResponse<Data | Error>
+) {
+    const { search } = req.body;
+
+    console.log("Search is >>> ", search);
+
+    res.status(200).json({
+        collection_id: "1234",
+        start_eta: 1234,
+    });
+}
