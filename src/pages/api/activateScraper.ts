967b4559753a34218adb6d5675c077ba593f7da5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-22 16:28:17 +0530

    updated

--- src/pages/api/activateScraper.ts
+++ src/pages/api/activateScraper.ts
@@ -9,13 +9,31 @@ type Error = {
     error: string;
 };
 
-export default function handler (
+export default async function handler (
     req: NextApiRequest,
     res: NextApiResponse<Data | Error>
 ) {
     const { search } = req.body;
 
     console.log("Search is >>> ", search);
+
+    const response = await fetch(
+        `https://api.brightdata.com/dca/trigger?
+        collector=c_lp6jyuug2eba476ja3&queue_next=1`, 
+        {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${process.env.BRIGHTDATA_API_KEY}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                search,
+            }),
+        }
+    );
+
+    const data = await response.json();
+    
 
     res.status(200).json({
         collection_id: "1234",
