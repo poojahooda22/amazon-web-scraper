e13e7779f62b295fa4797cef375c25be57faabfa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-23 11:38:54 +0530

    updated

--- pages/api/activateScraper.ts
+++ pages/api/activateScraper.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { adminDb } from "../../firebaseAdmin";
-import * as admin from 'firebase-admin';
+import  admin from 'firebase-admin';
 
 type Data = {
     collection_id: string;
@@ -19,11 +19,10 @@ export default async function handler (
 
     const { search } = req.body;
 
-    console.log("Search is >>> ", search);
+    console.log("Search is >>> ", search); 
 
     const response = await fetch(
-        `https://api.brightdata.com/dca/trigger?
-        collector=c_lp6jyuug2eba476ja3&queue_next=1`, 
+        `https://api.brightdata.com/dca/trigger?collector=c_lp6jyuug2eba476ja3&queue_next=1`, 
         {
             method: "POST",
             headers: {
@@ -41,16 +40,19 @@ export default async function handler (
 
     const { collection_id, start_eta } = data; 
 
+    if (typeof collection_id !== 'string' || collection_id.trim() === '') {
+        throw new Error('collection_id is not valid');
+    }
+
     await adminDb.collection('searches').doc(collection_id).set({
         search,
         start_eta,
         status: "pending",
         updatedAt: admin.firestore.Timestamp.now(),
-    
-    })
+    });
 
     return res.status(200).json({ collection_id, start_eta, });
-    } catch(err: any) {
+} catch(err: any) {
         console.log("Error is >>>", err);
         return res.status(500).json({ error: err.message });
     }
