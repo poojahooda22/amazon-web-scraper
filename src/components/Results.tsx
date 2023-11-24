91b95550e3b5ddbffeb5e405458ecb8df11112b8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-24 20:27:02 +0530

    updated

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -35,7 +35,8 @@ function Results({ results }: Props) {
                         {result.title}
                     </p>
                     <p 
-                        className='text-sm text-gray-500'>
+                        className='text-sm text-gray-500'
+                        >
                         {result.rating} ({result.reviews} reviews)
                     </p>
 
